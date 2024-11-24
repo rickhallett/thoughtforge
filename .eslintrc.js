11990eb1f62517c9de38cd4a84e6a6f01ef06bb5
Richard Hallett <rickhallett@live.co.uk>
2024-11-24 18:25:50 +0000

    feat: postgres setup docker

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,42 @@
- 
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    ecmaVersion: 2022,
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules', 'coverage'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', {
+      'argsIgnorePattern': '^_',
+      'varsIgnorePattern': '^_',
+    }],
+    'prettier/prettier': ['error', {
+      'endOfLine': 'auto',
+      'singleQuote': true,
+      'trailingComma': 'all',
+      'printWidth': 100,
+    }],
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'no-duplicate-imports': 'error',
+    'no-unused-expressions': 'error',
+    'prefer-const': 'error',
+    'eqeqeq': ['error', 'always'],
+    'curly': ['error', 'all'],
+  },
+}; 
